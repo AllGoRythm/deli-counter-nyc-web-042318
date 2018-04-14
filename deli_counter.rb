cbd05e01fc02740c638b7203c9974a40175d7f6e
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-14 14:32:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   elsif katz_deli.length >= 1
-    new_line
+    new_line = []
     katz_deli = "The line is currently:" + new_line.to_s
   end
 
