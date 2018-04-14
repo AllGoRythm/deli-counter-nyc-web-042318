13beff8c7e511817ae2a906d4d9e2dcf048d6059
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-14 13:52:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ i = 0
   while i < current.length
     my_line = my_line.push( "The line is currently: " + "#{i + 1}" + ". " + "#{current[i]}" + " ")
     i += 1
-    my_line.toString
+    my_line.to_s
     puts my_line
   end
 
