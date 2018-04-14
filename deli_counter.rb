c14d78e291e642c5296c8b26962b5b8d350b110b
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-14 14:33:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,12 @@ def line(katz_deli)
   elsif katz_deli.length >= 1
     new_line = []
     katz_deli = "The line is currently:" + new_line.to_s
-    
+
+    i = 0
+    while katz_deli.length > new_line
+
+    end
+
   end
 
 
