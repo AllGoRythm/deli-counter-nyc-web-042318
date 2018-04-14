7892b5bc840b9bdbbf22466afd36c940946be281
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-14 13:56:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,14 +3,6 @@ katz_deli = []
 def line(current)
   my_line = []
 
-i = 0
-  while i < current.length
-    my_line = my_line.push( "The line is currently: " + "#{i + 1}" + ". " + "#{current[i]}" + " ")
-    i += 1
-  
-    puts my_line
-  end
-
-  puts "The line is currently empty."
+  if current.length
 
 end
