7bdc6307b1c48146985e513660fd80125e3ce5d7
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-14 13:24:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,8 @@ def line(current)
   my_line = []
 
 i = 0
-  current.each do |person|
-    my_line << person[i]
-    i += 1
-    return my_line
+  while i < current.length  
+    my_line.push()
   end
 
   return "The line is currently empty."
