74a7f653415d30d12e51ecefd3245398fd99b8fa
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-14 14:05:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,12 @@ katz_deli = []
 
 def line(current)
   my_line = []
+  message = "The line is currently:" + my_line
 
   if current.length <= 0
     puts "The line is currently empty."
   elsif current.length > 0
-    
+    my_line.push("The line is currently:")
   end
 
 end
