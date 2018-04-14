53ed57f8d1573b5c2683be351e379328b9148774
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-14 14:19:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,10 +8,15 @@ def line(current)
     puts "The line is currently empty."
   end
 
-  i = 1
-  while katz_deli.length > 0
-    my_line.push("[i]" + ". " + "current[i]")
-    i += 1
-  end
-  return message
+
 end
+
+
+=begin
+i = 1
+while katz_deli.length > 0
+  my_line.push("[i]" + ". " + "current[i]")
+  i += 1
+end
+return message
+=end
